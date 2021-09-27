2ba273f4bdb950ae09220449cf30880c938467fa
Donard97 <donardgolaj@gmail.com>
2021-09-27 14:43:21 +0200

    Convert class based component to functional component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,7 @@
 import React from "react"
 
-class TodoItem extends React.Component {
-  render() {
-    return <li>{this.props.todo.title}</li>
-  }
+function TodoItem(props) {
+  return <li>{props.todo.title}</li>
 }
 
 export default TodoItem
