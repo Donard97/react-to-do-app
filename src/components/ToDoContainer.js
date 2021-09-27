a7d06f41019dacf1e7c1a9174011cb34bea5099c
Donard97 <donardgolaj@gmail.com>
2021-09-27 14:19:24 +0200

    Add an array of object to display items

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,13 +1,35 @@
 import React from "react"
 
 class TodoContainer extends React.Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+
   render() {
     return (
-    <React.Fragment>
-      <h1>Hello from Create React App</h1>
-      <p>I am in a React Component!</p>
-    </React.Fragment>
-    )
+      <ul>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+      ))}
+      </ul>
+    );
   }
 }
 export default TodoContainer
