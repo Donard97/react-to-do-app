fb0e52e16c9d3ac0b59066c387ecf72dd984d738
Donard97 <donardgolaj@gmail.com>
2021-09-28 15:49:03 +0200

    Add editing function

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -60,6 +60,17 @@ class TodoContainer extends React.Component {
     });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      }),
+    })
+  }
+
   render() {
     return (
       <div className="container">
@@ -67,9 +78,10 @@ class TodoContainer extends React.Component {
           <Header />
           <InputTodo addTodoProps={this.addTodoItem} />
           <TodosList
-            todos={this.state.todos}
-            handleChangeProps={this.handleChange}
-            deleteTodoProps={this.delTodo}
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+          setUpdate={this.setUpdate}
           />
         </div>
       </div>
