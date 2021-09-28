e0b8cab9247dec0b34e5cf0942236d2993fbf822
Donard97 <donardgolaj@gmail.com>
2021-09-28 21:22:12 +0200

    Deploy pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom"
 import TodoContainer from "./functionBased/components/ToDoContainer";
 import "./functionBased/app.css";
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
