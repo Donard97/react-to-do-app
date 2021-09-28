0f9e7ff24e2f7a05880f7bf556a245ad97808ad1
Donard97 <donardgolaj@gmail.com>
2021-09-28 12:17:51 +0200

    Add form, unique ids and validation

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import TodosList from './ToDoList';
 import Header from "./Header"
+import InputTodo from "./InputTodo"
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Setup development environment',
         completed: true,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Develop website and add content',
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Deploy to live server',
         completed: false,
       },
@@ -47,10 +49,22 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList todos={this.state.todos}
         handleChangeProps={this.handleChange} 
         deleteTodoProps={this.delTodo} />
