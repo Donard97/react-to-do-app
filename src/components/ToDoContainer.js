7747530c125aebe316cfcaa539173eb25451b65f
Donard97 <donardgolaj@gmail.com>
2021-09-28 11:41:44 +0200

    Add handlers and delete button

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -23,11 +23,37 @@ class TodoContainer extends React.Component {
     ],
   };
 
+  handleChange = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      }),
+    }))
+  }
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList todos={this.state.todos}
+        handleChangeProps={this.handleChange} 
+        deleteTodoProps={this.delTodo} />
       </div>
     );
   }
